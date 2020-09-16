8145b973e5bf82bb59c05439a3a4b04a55cb316e
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-09-16 13:34:30 -0400

    adds program to determine pay with overtime

---
+++ hourly_rate_program.py
@@ -0,0 +1,21 @@
+# hrs = input("Enter Hours: ")
+# rate = input("Enter Rate: ")
+
+# pay = float(hrs) * float(rate)
+
+# print("Pay: ", pay)
+
+hours = input("Enter Hours: ")
+h = float(hours)
+rate = input("Enter your rate per hour: ")
+r = float(rate)
+overtimeRate = 1.5 * r
+hourlyRate = h * r
+
+if h > 40 :
+  overtimeHours = h - 40
+  overtimeTotal = overtimeHours * overtimeRate + 420
+  print(overtimeTotal)
+else :
+  print("Your pay for the week is:", h * r)
+  
