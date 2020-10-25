c387adfffb09aa61c0d70216ace6b2f04f65a575
Justin Crabtree <54284826+justin-crabtree@users.noreply.github.com>
2020-10-25 15:44:58 -0400

    changed variable to sum_data

--- open_file_test.py
+++ open_file_test.py
@@ -2,15 +2,19 @@
 # fh = open(fname)
 # print(fh.read().upper().rstrip())
 
+# Write a program that prompts for a file name, then opens that file and reads through the file, looking for lines of the form:
+# X-DSPAM-Confidence:    0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+
 
 fname = input("Enter file name: ")
 fh = open(fname)
-added_data = 0
+sum_data = 0
 count = 0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:") : continue
     count = count + 1 
     num_data = float(line[20:])
-    added_data = num_data + added_data
+    sum_data = num_data + sum_data
 
-print("Average spam confidence:", added_data / count)
+print("Average spam confidence:", sum_data / count)
